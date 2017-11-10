6b382961aaef622ee51eedf58abf0f103a1e0b62
rxwen <rx.wen218@gmail.com>
2017-11-10 17:35:45 +0800

    fix missing of module tags

--- Android.mk
+++ Android.mk
@@ -27,7 +27,9 @@ ifneq ($(TARGET_SIMULATOR),true)
 LOCAL_C_INCLUDES += bionic		# very important!
 LOCAL_C_INCLUDES += external/stlport/stlport
 LOCAL_SHARED_LIBRARIES += libstlport
-else
-LOCAL_LDLIBS += -lpthread
 endif
+
+LOCAL_PRELINK_MODULE := false
+LOCAL_MODULE_TAGS := eng
+
 include $(BUILD_SHARED_LIBRARY)
